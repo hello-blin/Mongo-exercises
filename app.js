8e2e97cd3470d83dd54a0f03e645500a6e6a2b9e
Blin Kukaj <kblin164@gmail.com>
2022-04-01 03:20:05 +0200

    Starting on writing logical query operators

--- app.js
+++ app.js
@@ -109,25 +109,39 @@ try {
 
 /////////////////////////////
 
-
 /////////////////////////////
 
 //QUERYING BY PRICE TO SHOW COURSES THAT MATCH THE VALUES 28, 55, 65
 
-async function getCoursesByComparisionOperators() {
-  const courses = await Course.find({ price: { $in: [32, 28, 65, 55, 43] } })
-    .limit(10)
-    .sort({ author: 1 })
-    .select({ name: 1, tags: 1, price: 1 });
-  console.log(courses);
-}
+// async function getCoursesByComparisionOperators() {
+//   const courses = await Course.find({ price: { $in: [32, 28, 65, 55, 43] } })
+//     .limit(10)
+//     .sort({ author: 1 })
+//     .select({ name: 1, tags: 1, price: 1 });
+//   console.log(courses);
+// }
 
-getCoursesByComparisionOperators();
+// getCoursesByComparisionOperators();
 
 //QUERYING BY PRICE TO SHOW COURSES GREATER THAN 22$
 
 /////////////////////////////
 
+//----------------------------------------------------------------LOGICAL QUERY OPERATORS----------------------------------------------------------------
+
+async function getCourse() {
+  //or
+  //and
+
+  const courses = await Course.find()
+    .or()
+    .sort({ price: 1, name: 1 })
+    .select({ price: 1, name: 1, tags: 1 });
+
+  console.log(courses);
+}
+
+//----------------------------------------------------------------LOGICAL QUERY OPERATORS----------------------------------------------------------------
 
 app.get("/", (req, res) => {
   res.send("Hi");
