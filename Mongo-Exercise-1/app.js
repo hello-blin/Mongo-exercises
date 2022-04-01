536d4a2b53f3f8e01bc4f77d2c91d162c8e704f5
Blin Kukaj <kblin164@gmail.com>
2022-04-01 04:05:25 +0200

    Innitial Commit

---
+++ Mongo-Exercise-1/app.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+
+const courseSchema = new mongoose.Schema({
+  name: String,
+  author: String,
+  price: Number,
+  tags: [String],
+  isPublished: Boolean,
+});
+
+const Course = new mongoose.model("Course", courseSchema);
+
+app.listen(3000, () => {
+  console.log("Connected to 3000");
+});
