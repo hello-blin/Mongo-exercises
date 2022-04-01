79d7bd0e3c1136b65639049ade408c4425f9cb68
Blin Kukaj <kblin164@gmail.com>
2022-04-01 04:07:00 +0200

    Empty

--- Mongo-Exercise-1/app.js
+++ Mongo-Exercise-1/app.js
@@ -12,6 +12,14 @@ const courseSchema = new mongoose.Schema({
 
 const Course = new mongoose.model("Course", courseSchema);
 
+
+
+//EXERCISE 1 
+
+async function getCourse(){
+
+}
+
 app.listen(3000, () => {
   console.log("Connected to 3000");
 });
